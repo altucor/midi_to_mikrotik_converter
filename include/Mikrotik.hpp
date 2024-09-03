13bb7466b7f44bd9ae04c8fecbab740e43cf85b6
Sergey Nadtochii <serega96916@gmail.com>
2024-09-04 02:36:19 +0300

    switching to libmidi

--- include/Mikrotik.hpp
+++ include/Mikrotik.hpp
@@ -1,38 +1,43 @@
 #ifndef MIKROTIK_HPP
 #define MIKROTIK_HPP
 
-#include "MidiTrack.hpp"
+#include "mtrk.h"
 #include "Note.hpp"
 #include <vector>
 
 class Mikrotik
 {
 public:
-	Mikrotik(MidiTrack &track, 
-		const uint64_t index, 
-		const int octaveShift, 
-		const int noteShift, 
-		const double fineTuning,
-		const bool commentsFlag
-	);
+	Mikrotik(mtrk_t *track,
+			 const uint16_t ppqn,
+			 const int32_t bpm,
+			 const uint64_t index,
+			 const int octaveShift,
+			 const int noteShift,
+			 const double fineTuning,
+			 const bool commentsFlag);
 	int buildScriptForChannel(std::string &fileName, const uint8_t channel);
 	int buildScript(std::string &fileName);
+
 private:
 	std::string getTimeAsText(const double time);
 	std::string getTrackTimeLength(const uint8_t channel);
 	std::string getNotesCount(const uint8_t channel);
 	std::string getScriptHeader(const uint8_t channel);
-	std::string getDelayLine(const double delayMs);
-	std::string getBeepLine(Note note);
-	std::string buildNote(Note note);
+	std::string getDelayLine(const float delayMs);
+	std::string getBeepLine(midi_note_t note, const float duration);
+	// std::string buildNote(Note note);
+
 private:
 	bool m_commentsFlag = false;
+	uint16_t m_ppqn;
+	int32_t m_bpm;
 	uint64_t m_index = 0;
 	int m_octaveShift = 0;
 	int m_noteShift = 0;
 	double m_fineTuning = 0.0;
 	double m_processedTime = 0.0;
-	MidiTrack m_track;
+	mtrk_t *m_track;
 };
 
 #endif // MIKROTIK_HPP
--- src/midi-to-mikrotik-converter.cpp
+++ src/midi-to-mikrotik-converter.cpp
@@ -21,7 +21,8 @@
 #include <cstdio>
 #include <vector>
 
-#include "MidiFile.hpp"
+// #include "MidiFile.hpp"
+#include "file.h"
 #include "Mikrotik.hpp"
 
 #include <boost/program_options.hpp>
@@ -34,34 +35,31 @@ static void init_log()
 {
 	static const std::string COMMON_FMT("[%TimeStamp%][%Severity%]: %Message%");
 
-	boost::log::register_simple_formatter_factory< boost::log::trivial::severity_level, char >("Severity");
+	boost::log::register_simple_formatter_factory<boost::log::trivial::severity_level, char>("Severity");
 
 	// Output message to console
 	boost::log::add_console_log(
 		std::cout,
 		boost::log::keywords::format = COMMON_FMT,
-		boost::log::keywords::auto_flush = true
-	);
+		boost::log::keywords::auto_flush = true);
 
-	// Output message to file, rotates when file 
+	// Output message to file, rotates when file
 	// reached 1mb or at midnight every day. Each log file
 	// is capped at 1mb and total is 20mb
-	boost::log::add_file_log (
+	boost::log::add_file_log(
 		boost::log::keywords::file_name = "mtmc_%3N.log",
 		boost::log::keywords::rotation_size = 100 * 1024 * 1024,
 		boost::log::keywords::max_size = 100 * 1024 * 1024,
 		boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0),
 		boost::log::keywords::format = COMMON_FMT,
-		boost::log::keywords::auto_flush = true
-	);
+		boost::log::keywords::auto_flush = true);
 
 	boost::log::add_common_attributes();
 
 	// Only output message with INFO or higher severity in Release
 #ifndef _DEBUG
 	boost::log::core::get()->set_filter(
-		boost::log::trivial::severity >= boost::log::trivial::info
-	);
+		boost::log::trivial::severity >= boost::log::trivial::info);
 #endif
 }
 
@@ -69,28 +67,19 @@ int main(int argc, char *argv[])
 {
 	init_log();
 
-	int result = 0;
-	int octaveShift = 0;
-	int noteShift = 0;
+	int32_t result = 0;
+	int32_t octaveShift = 0;
+	int32_t noteShift = 0;
 	std::string inFileName = "";
 	std::string outFileName = "";
-	int newBpm = 0;
+	int32_t newBpm = 0;
 	bool enableCommentsFlag = false;
 	double fineTuning = 0.0;
 
 	po::options_description desc("Application arguments");
-	desc.add_options()
-		("help,h", "Print this help message")
-		("file,f", po::value<std::string>(&inFileName), "Select input standart midi file (SMF)")
-		("output-file,g", po::value<std::string>(&outFileName), "Specify output file name. If not set than output will be saved in file with same name as input but with additional suffix")
-		("octave-shift,o", po::value<int>(&octaveShift), "Sets the octave shift relative to the original (-10 to +10)")
-		("note-shift,n", po::value<int>(&noteShift), "Sets the note shift relative to the original")
-		("fine-tuning,t", po::value<double>(&fineTuning), "Sets frequency offset for all notes in case when you think your beeper produces beeps at wrong frequencies")
-		("bpm,b", po::value<int>(&newBpm), "Sets the new bpm to output file")
-		("comments,c", "Adds comments in the form of notes")
-	;
+	desc.add_options()("help,h", "Print this help message")("file,f", po::value<std::string>(&inFileName), "Select input standart midi file (SMF)")("output-file,g", po::value<std::string>(&outFileName), "Specify output file name. If not set than output will be saved in file with same name as input but with additional suffix")("octave-shift,o", po::value<int>(&octaveShift), "Sets the octave shift relative to the original (-10 to +10)")("note-shift,n", po::value<int>(&noteShift), "Sets the note shift relative to the original")("fine-tuning,t", po::value<double>(&fineTuning), "Sets frequency offset for all notes in case when you think your beeper produces beeps at wrong frequencies")("bpm,b", po::value<int>(&newBpm), "Sets the new bpm to output file")("comments,c", "Adds comments in the form of notes");
 
-	if(argc == 1)
+	if (argc == 1)
 	{
 		std::cout << desc << std::endl;
 		return 0;
@@ -103,55 +92,109 @@ int main(int argc, char *argv[])
 		po::store(parsed, vm);
 		po::notify(vm);
 
-		if(vm.count("help"))
+		if (vm.count("help"))
 		{
 			std::cout << desc << std::endl;
 			return 0;
 		}
-		if(vm.count("comments"))
+		if (vm.count("comments"))
 			enableCommentsFlag = true;
 	}
-	catch(std::exception& ex)
+	catch (std::exception &ex)
 	{
 		std::cout << ex.what() << std::endl;
 		std::cout << desc << std::endl;
 	}
 
-	/*
-	BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
-	BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
-	BOOST_LOG_TRIVIAL(info) << "An informational severity message";
-	BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
-	BOOST_LOG_TRIVIAL(error) << "An error severity message";
-	BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
-	*/
-
-	if(inFileName == "")
+	if (inFileName == "")
 	{
 		BOOST_LOG_TRIVIAL(error) << "No input (SMF) file specified";
 		std::cout << desc << std::endl;
-		return 0;
+		return -1;
 	}
 
-	if(outFileName == "")
-		outFileName = inFileName;
-
-	MidiFile midiObj(inFileName, newBpm);
-	midiObj.process();
-	std::vector<MidiTrack> tracks = midiObj.getTracks();
-
-	for(uint64_t i=0; i<tracks.size(); i++)
+	if (outFileName == "")
 	{
+		outFileName = inFileName;
+	}
+
+	std::ifstream midiStream{inFileName, midiStream.binary | midiStream.in};
+	if (!midiStream.is_open())
+	{
+		BOOST_LOG_TRIVIAL(error) << "Cannot open file: " << inFileName;
+		return -1;
+	}
+	std::vector<unsigned char> midiFileBuffer(std::istreambuf_iterator<char>(midiStream), {});
+
+	midi_file_t *midiFile = midi_file_new();
+
+	int read = midi_file_unmarshal(midiFile, midiFileBuffer.data(), midiFileBuffer.size());
+	if (read > 0)
+	{
+		BOOST_LOG_TRIVIAL(info) << "Midi file parsed total bytes: " << std::to_string(read) << std::endl;
+	}
+	else
+	{
+		BOOST_LOG_TRIVIAL(error) << "Error parsing midi file stream: " << std::to_string(read) << std::endl;
+		midi_file_free(midiFile);
+		return -1;
+	}
+
+	uint16_t ppqn = midi_file_get_mthd(midiFile).ppqn;
+
+	// If new not set by user input find in one of tracks
+	if (newBpm == 0)
+	{
+		auto trackCount = midi_file_get_tracks_count(midiFile);
+		for (uint32_t i = 0; i < trackCount; i++)
+		{
+			mtrk_t *track = midi_file_get_track(midiFile, i);
+
+			midi_cmd_t cmd = {.status = MIDI_STATUS_SYSTEM, .subCmd = MIDI_STATUS_SYSTEM_RESET_OR_META};
+			uint8_t message_meta = MIDI_META_EVENT_TEMPO;
+			uint32_t bpmIndex = mtrk_find_event_index(track, 0, cmd, message_meta);
+			if (bpmIndex != -1)
+			{
+				midi_event_smf_t *bpmEvent = mtrk_get_event(track, bpmIndex);
+				newBpm = bpmEvent->event.meta.tempo.val;
+			}
+		}
+	}
+
+	auto trackCount = midi_file_get_tracks_count(midiFile);
+
+	for (uint32_t i = 0; i < trackCount; i++)
+	{
+		mtrk_t *track = midi_file_get_track(midiFile, i);
 		Mikrotik mikrotik(
-			tracks[i], 
-			i, 
-			octaveShift, 
-			noteShift, 
+			track,
+			ppqn,
+			newBpm,
+			i,
+			octaveShift,
+			noteShift,
 			fineTuning,
-			enableCommentsFlag
-		);
+			enableCommentsFlag);
 		mikrotik.buildScript(outFileName);
 	}
+
+	midi_file_free(midiFile);
+
+	// MidiFile midiObj(inFileName, newBpm);
+	// midiObj.process();
+	// std::vector<MidiTrack> tracks = midiObj.getTracks();
+
+	// for (uint64_t i = 0; i < tracks.size(); i++)
+	// {
+	// 	Mikrotik mikrotik(
+	// 		tracks[i],
+	// 		i,
+	// 		octaveShift,
+	// 		noteShift,
+	// 		fineTuning,
+	// 		enableCommentsFlag);
+	// 	mikrotik.buildScript(outFileName);
+	// }
 
 	return 0;
 }
